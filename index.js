5d07cc72234f6dacd8fedcdc29a7394efa3f5117
NotArtwork <105832230+NotArtwork@users.noreply.github.com>
2022-06-14 16:46:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+   const new_cats = [
+    ...cats.slice(0),
+    name
+    ]
+    return new_cats
+}
+
+function prependCat(name) {
+    const new_cats = [
+        name,
+     ...cats.slice(0),
+     
+     ]
+     return new_cats
+ }
+ 
+ function removeLastCat() {
+    const new_cats = [ 
+        ...cats.slice(0, -1)
+    ]
+     return new_cats
+ }
+
+ function removeFirstCat() {
+    const new_cats = [
+        ...cats.slice(1)
+    ]
+    return new_cats
+ }
+ 
